a1a13ed97309327db8683d77eb9f6cd8a03e0f59
buithihatien2711 <buithihatien2711@gmail.com>
2022-12-11 10:37:33 +0700

    Admin update user

--- RentalCar.API/Controllers/AdminController.cs
+++ RentalCar.API/Controllers/AdminController.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using RentalCar.API.Models;
 using RentalCar.Model.Models;
@@ -11,51 +14,23 @@ namespace RentalCar.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
-    public class AccountController : ControllerBase
+    public class AdminController : ControllerBase
     {
         private readonly IUserService _userService;
         private readonly IMapper _mapper;
-        private readonly ICarService _carService;
         private readonly IUploadImgService _uploadImgService;
 
-        public AccountController(IUserService userService, IMapper mapper, ICarService carService, IUploadImgService uploadImgService)
+        public AdminController(IUserService userService, IMapper mapper, IUploadImgService uploadImgService)
         {
             _uploadImgService = uploadImgService;
-            _carService = carService;
-            _userService = userService;
             _mapper = mapper;
+            _userService = userService;
+            
         }
         
-        [Route("/api/profile/me")]
-        [HttpGet]
-        public ActionResult<UserProfile> Get()
-        {
-            var username = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
-            // var username = "nguyenvana";
-            var user = _userService.GetUserByUsername(username);
-            if (user == null) return NotFound();
-
-            var profile = _mapper.Map<User, UserProfile>(user);
-            return Ok(profile);
-        }
-
-        [HttpPatch("/api/account")]
-        public ActionResult<UserProfile> UpdateUserPatch(JsonPatchDocument userModel)
-        {
-            var username = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
-            // var username = "nguyenvana";
-
-            if(string.IsNullOrEmpty(username)) return NotFound();
-
-            _userService.UpdateUserPatch(username, userModel);
-
-            if(_userService.SaveChanges()) return Ok(_mapper.Map <User, UserProfile>(_userService.GetUserByUsername(username)));
-            return BadRequest("Profile update failed");
-        }
-
-        [HttpPut("/api/account/avatar")]
-        public async Task<ActionResult<UserProfile>> UpdateAvatar(IFormFile userAvatar)
+        [Authorize]
+        [HttpPut("/api/admin/account")]
+        public ActionResult<UpdateUserDto> Put(UpdateUserDto userProfile)
         {
             var username = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
             // var username = "nguyenvana";
@@ -64,25 +39,57 @@ namespace RentalCar.API.Controllers
 
             var userExist = _userService.GetUserByUsername(username);
 
-            userExist.ProfileImage = await _uploadImgService.UploadImage("avatar", username, userAvatar);
+            _mapper.Map<UpdateUserDto, User>(userProfile, userExist);
 
             _userService.UpdateUser(username, userExist);
 
             if (_userService.SaveChanges())
             {
-                Dictionary<string, string> response = new Dictionary<string, string>();
-                response.Add("avatar", userExist.ProfileImage);
-                return Ok(response);
+                var admin = _userService.GetUserByUsername(username);
+                if(admin == null) return null;
+                return Ok(_mapper.Map<User, AdminProfileDto>(admin));
             } 
-            return BadRequest("Avatar update failed");
+            return BadRequest("Profile update failed");
         }
 
-        [HttpPut("/api/account/license")]
-        public async Task<ActionResult<LicenseViewDto>> UpdateLicense([FromForm] LicenseDto licenseDto)
+        [Route("/api/admin/profile/me")]
+        [HttpGet]
+        public ActionResult<AdminProfileDto> GetProfileAdmin()
         {
             var username = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
             // var username = "nguyenvana";
+            var user = _userService.GetUserByUsername(username);
+            if (user == null) return NotFound();
 
+            var profile = _mapper.Map<User, AdminProfileDto>(user);
+            return Ok(profile);
+        }
+
+        [Authorize(Roles="admin")]
+        [HttpPut("/api/admin/account/{username}")]
+        public ActionResult<UpdateUserDto> UpdateUserProfile(UpdateUserDto userProfile, string username)
+        {
+            if(string.IsNullOrEmpty(username)) return NotFound();
+
+            var userExist = _userService.GetUserByUsername(username);
+
+            _mapper.Map<UpdateUserDto, User>(userProfile, userExist);
+
+            _userService.UpdateUser(username, userExist);
+
+            if (_userService.SaveChanges())
+            {
+                var admin = _userService.GetUserByUsername(username);
+                if(admin == null) return null;
+                return Ok(_mapper.Map<User, UpdateUserDto>(admin));
+            } 
+            return BadRequest("Profile update failed");
+        }
+
+        [Authorize(Roles="admin")]
+        [HttpPut("/api/admin/account/license/{username}")]
+        public async Task<ActionResult<LicenseViewDto>> UpdateLicense([FromForm] LicenseDto licenseDto, string username)
+        {
             if(string.IsNullOrEmpty(username)) return NotFound();
 
             var userLicenseExist = _userService.GetUserByUsername(username).License;
